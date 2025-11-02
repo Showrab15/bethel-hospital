a9a35b734ff85bb8115e56dadcf9ebaecbc9e956
Showrab <supto50showrab@gmail.com>
2025-11-02 14:49:17 +0600

    unsed code removed

--- src/components/Banner/MobileBanner.jsx
+++ src/components/Banner/MobileBanner.jsx
@@ -1,5 +1,4 @@
 
-import curveBackground from "../../assets/curve1.png"; // Replace with the path to your curved background image
 import home from "../../assets/Group 500.png";
 import home2 from "../../assets/rectangle2.png";
 import Navbar from "./../Navbar/Navbar";
